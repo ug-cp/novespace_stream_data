16453a022353cf3d7b65388dfc1baace545f6e02
Daniel Mohr <daniel.mohr@dlr.de>
2025-11-03 11:17:50 +0100

    enhanced coding style

--- nove_space_stream.py
+++ nove_space_stream.py
@@ -26,10 +26,16 @@ class NoSpaStream():
     which is provided in scrientific research flights.
 
     This stream data was first provided during
-    45. DLR parabolic flight campaign at October 2025.
+    45. DLR parabolic flight campaign in October 2025.
     """
-    # pylint: disable too-many-instance-attributes
+    # pylint: disable=too-many-instance-attributes
     def __init__(self, csv_path, inputport="3131", printing=False):
+        """
+        :param csv_path: path to store the data
+        :param inputport: port to listen.
+        :param printing: If set to True the data is not only logged, but also
+                         printed on the console (stdout).
+        """
         self.streampath = csv_path
         self.streamport = inputport
         self.print_on_console = printing
@@ -40,17 +46,26 @@ class NoSpaStream():
             ' Time', ' Jx (g)', ' Jy (g)', ' Jz (g)',
             ' Temperature (°C)', ' Humidity (%)', ' Pressure (mbar)',
             ' Parabola', ' Announcement']
+        self.socket = None
+        self.csv_file = None
+        self.streaming_thread = None
 
         if not os.path.isdir(self.streampath):
             print("ERROR! No valid folder for csv-file is given.")
 
     def connect_socket(self):
+        """
+        Connect to the UDP socket.
+        """
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.socket.bind(self.socket_adress)
         print(
             f"Creation of UDP-socket with port {self.streamport} sucessfull.")
 
     def get_status(self):
+        """
+        Print the current streaming status.
+        """
         if self.streaming_status is True:
             print(
                 "Active datastream from port "
@@ -59,6 +74,9 @@ class NoSpaStream():
             print("No active datastream!")
 
     def start_streaming(self):
+        """
+        Start the data streaming process.
+        """
         if self.streaming_status is True:
             print("The stream is allready active!")
         else:
@@ -75,10 +93,10 @@ class NoSpaStream():
             self.csv_file.touch()
             with open(self.csv_file, mode='a',
                       newline='', encoding='utf-8') as filedescriptor:
-                self.csv_writer = csv.DictWriter(
+                csv_writer = csv.DictWriter(
                     filedescriptor,
                     fieldnames=self.csv_fieldnames, delimiter=';')
-                self.csv_writer.writeheader()
+                csv_writer.writeheader()
             print(
                 "Starting datastream from port "
                 f"{self.streamport} to file: {self.csv_file}")
@@ -89,6 +107,9 @@ class NoSpaStream():
             self.streaming_thread.start()
 
     def end_streaming(self):
+        """
+        End the data streaming process.
+        """
         if self.streaming_status is False:
             print("No active data-stream to be terminated.")
         else:
@@ -100,32 +121,35 @@ class NoSpaStream():
                 print(datetime.now().strftime(
                     "Streaming ended on %Y-%m-%d at %H:%M:%S"))
                 sys.exit(0)
-            except:  # noqa: E722
+            except:  # noqa: E722 pylint: disable=W0702
                 print("ERROR! Ending of Datastream was not sucessfull!")
                 sys.exit(1)
 
     def stream_data(self):
+        """
+        Receive and save data from the UDP socket.
+        """
         print("streaming data\n")
         try:
             while self.streaming_status is True:
                 # Recive and decode datastream
-                self.data, self.address = self.socket.recvfrom(1024)
-                self.data_str = self.data.decode('utf-8')
+                data, _ = self.socket.recvfrom(1024)
+                data_str = data.decode('utf-8')
                 unixtime = str(time.time())
-                self.data_str = unixtime + ';' + self.data_str
+                data_str = unixtime + ';' + data_str
 
                 # Save the data into the csv file
                 with open(self.csv_file, mode='a',
                           newline='', encoding='utf-8') as filedescriptor:
-                    self.csv_writer = csv.writer(filedescriptor)
-                    # print(self.data_str)
-                    self.data_str2 = ['1', '2', '3', '4',
+                    csv_writer = csv.writer(filedescriptor)
+                    # print(data_str)
+                    data_str2 = ['1', '2', '3', '4',
                                       '5', '6', '7', '8', '9', '10']
-                    self.data_str2.insert(0, unixtime)
-                    self.csv_writer.writerow([self.data_str])
-                    # self.csv_writer.writerow([self.data_str2])
+                    data_str2.insert(0, unixtime)
+                    csv_writer.writerow([data_str])
+                    # csv_writer.writerow([data_str2])
                     if self.print_on_console is True:
-                        print(self.data_str)
+                        print(data_str)
         except KeyboardInterrupt:
             # print(f"Error in receiving data: {e}")
             self.end_streaming()
