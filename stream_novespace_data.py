95156c85156cfcc6e4c8df3a49544270272d6935
Daniel Mohr <daniel.mohr@dlr.de>
2025-11-03 15:25:03 +0100

    added license GPL-3.0-or-later

--- stream_novespace_data.py
+++ stream_novespace_data.py
@@ -1,11 +1,31 @@
+# SPDX-FileCopyrightText: 2025 Daniel Maier, Daniel Mohr, Thomas Villatte
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
 """
-Created on Thu Oct 23 11:04:46 2025
+`novespace_stream_data` gets the stream from Novespace during
+scientific research flights.
 
-@author: daniel_m
+Copyright (C) 2025 Daniel Maier (University of Greifswald),
+                   Daniel Mohr (University of Greifswald),
+                   Thomas Villatte (Novespace)
 
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
 import os
+
 import nove_space_stream
 
 
